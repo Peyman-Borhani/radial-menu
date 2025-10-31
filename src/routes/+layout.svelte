a10c9a9adf5433824f55e263118eecdcdda05553
peyman-borhani <peyman.borhani@gmail.com>
2025-10-31 12:16:39 +0330

    ~Code-update:  latest versions migration => +Fix: @tabler import path +Remove: deprecated => csr export

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '$/styles/index.scss';
-	import '@tabler/icons-webfont/tabler-icons.min.css';
+	import '@tabler/icons-webfont/dist/tabler-icons.min.css';
 	interface Props {children?: import('svelte').Snippet}
 
 	let { children }: Props = $props();
 
-	export const csr = true;
-	export const ssr = false;
+	export const  ssr  = false;	
+    //export const csr =true;  //Deprecated
 </script>
 
 {@render children?.()}
